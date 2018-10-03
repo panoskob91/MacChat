2715205199cd91b4bef855c86e0c7228addde2ff
Panagiotis  Kompotis <panoskob91@gmail.com>
2018-10-03 08:23:10 +0300

    Initial Commit

---
+++ Chat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chat
+//
+//  Created by Panagiotis  Kompotis  on 03/10/2018.
+//  Copyright © 2018 Panagiotis  Kompotis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
