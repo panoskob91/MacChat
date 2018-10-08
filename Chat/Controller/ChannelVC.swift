e575fe8d03e2240bd2c512978f370606d673d807
Panagiotis  Kompotis <panoskob91@gmail.com>
2018-10-08 08:01:33 +0300

    Initial commit

---
+++ Chat/Controller/ChannelVC.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelVC.swift
+//  Chat
+//
+//  Created by Panagiotis  Kompotis  on 08/10/2018.
+//  Copyright © 2018 Panagiotis  Kompotis. All rights reserved.
+//
+
+import Cocoa
+
+class ChannelVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Chat/Controller/ToolbarVC.swift
@@ -0,0 +1,18 @@
+//
+//  ToolbarVC.swift
+//  Chat
+//
+//  Created by Panagiotis  Kompotis  on 08/10/2018.
+//  Copyright © 2018 Panagiotis  Kompotis. All rights reserved.
+//
+
+import Cocoa
+
+class ToolbarVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
