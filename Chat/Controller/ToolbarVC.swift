04c57f926c97a4c4601b705a2f51b96651f230e5
Panagiotis  Kompotis <panoskob91@gmail.com>
2018-10-08 20:46:13 +0300

    Assets and login label added

--- Chat/Controller/ToolbarVC.swift
+++ Chat/Controller/ToolbarVC.swift
@@ -10,10 +10,21 @@ import Cocoa
 
 class ToolbarVC: NSViewController {
 
+    
+    @IBOutlet var userAvatar: NSImageView!
+    @IBOutlet var loginLabel: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.wantsLayer = true
         self.view.layer?.backgroundColor = toolbarColor.cgColor
+        setupLoginLabel()
+    }
+
+    func setupLoginLabel()
+    {
+        self.loginLabel.textColor = loginLabelColor
+        self.loginLabel.font = NSFont(name: "Avenir Next Regular", size: 15)
     }
     
 }
--- Chat/Utilities/Constants.swift
+++ Chat/Utilities/Constants.swift
@@ -10,6 +10,6 @@ import Cocoa
 
 //Colors
 let toolbarColor = NSColor.createColor(red: 0, green: 102, blue: 255, alpha: 1.0)
-//let chatColor = NSColor.createColor(red: 204, green: 204, blue: 255, alpha: 1.0)
 let chatColor = NSColor.createColor(red: 230, green: 230, blue: 230, alpha: 1.0)
 let channelColor = NSColor.createColor(red: 179, green: 0, blue: 71, alpha: 1.0)
+let loginLabelColor = NSColor.createColor(red: 204, green: 204, blue: 204, alpha: 1.0)
