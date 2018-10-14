45e5f715ad918e550ad37374fe49fca1e626dcb0
Panagiotis  Kompotis <panoskob91@gmail.com>
2018-10-14 17:14:35 +0300

    Modal login

--- Chat/View/ModalLogin.swift
+++ Chat/View/ModalLogin.swift
@@ -12,11 +12,16 @@ class ModalLogin: NSView {
 
     //MARK:- IBOutlets
     @IBOutlet weak var view: NSView!
+    @IBOutlet var signInLabel: NSTextField!
+    @IBOutlet var emailTextField: NSTextField!
+    @IBOutlet var passwordTextField: NSSecureTextField!
+    @IBOutlet var loginButton: NSButton!
+    @IBOutlet var createAnAccountButton: NSButton!
+    @IBOutlet var stackView: NSStackView!
     
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         Bundle.main.loadNibNamed(NSNib.Name(rawValue: "ModalLogin"), owner: self, topLevelObjects: nil)
-        self.view.frame = NSRect(x: 0, y: 0, width: 475, height: 300)
         self.addSubview(self.view)
     }
     
@@ -31,7 +36,47 @@ class ModalLogin: NSView {
     
     func setupView()
     {
+        self.view.frame = NSRect(x: 0, y: 0, width: 475, height: 300)
         self.view.layer?.backgroundColor = CGColor.white
         self.view.layer?.cornerRadius = 7
+        
+        self.signInLabel.alignment = .center
+        self.signInLabel.textColor = NSColor.black
+        self.signInLabel.font = NSFont(name: AVENIR_BOLD, size: loginFontSize)
+        
+        self.emailTextField.placeholderString = "Email"
+        self.emailTextField.layer?.cornerRadius = 5
+        self.emailTextField.textColor = NSColor.darkGray
+        self.emailTextField.isBordered = false
+        
+        self.passwordTextField.placeholderString = "Password"
+        self.passwordTextField.layer?.cornerRadius = 5
+        self.passwordTextField.textColor = NSColor.darkGray
+        self.passwordTextField.isBordered = false
+        
+        let modalGreen = NSColor.createColor(red: 0, green: 153, blue: 51, alpha: 1.0)
+        self.loginButton.layer?.backgroundColor = modalGreen.cgColor
+        self.loginButton.title = "Log in"
+        self.loginButton.font = loginFont
+        self.loginButton.layer?.cornerRadius = 7
+        self.loginButton.setTitleColor(color: NSColor.white)
+        self.loginButton.isBordered = false
+        
+        self.createAnAccountButton.font = loginFont
+        self.createAnAccountButton.title = "Create an account"
+        self.createAnAccountButton.setTitleColor(color: modalGreen)
+        self.createAnAccountButton.isBordered = false
     }
+    
+    //MARK:- IBActions
+    @IBAction func closeModalClicked(_ sender: NSButton)
+    {
+        
+    }
+    @IBAction func createAccountButtonClicked(_ sender: NSButton)
+    {
+        
+    }
+    
+    
 }
